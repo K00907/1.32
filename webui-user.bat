e4df82f261727c036dcb1d3ee9339bf7b401f15d
dsxailab <dsxailab@noreply.com>
2023-06-17 01:25:15 +0800

    move the torch2.0, which deprecates xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--listen --api --xformers --enable-insecure-extension-access
+set COMMANDLINE_ARGS=--listen --api --enable-insecure-extension-access --opt-sdp-attention
 
 call webui.bat
